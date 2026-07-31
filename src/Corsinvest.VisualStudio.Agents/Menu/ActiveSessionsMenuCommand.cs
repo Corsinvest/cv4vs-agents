158929f975dc1a449bb85a471cfeddec67aaa577
Daniele Corsini <daniele.corsini@corsinvest.it>
2026-07-31 10:34:20 +0200

    chore: drop two unused System.Linq usings and their files' BOM

--- src/Corsinvest.VisualStudio.Agents/Menu/ActiveSessionsMenuCommand.cs
+++ src/Corsinvest.VisualStudio.Agents/Menu/ActiveSessionsMenuCommand.cs
@@ -8,7 +8,6 @@ using Microsoft.VisualStudio.Shell;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
-using System.Linq;
 using Task = System.Threading.Tasks.Task;
 
 namespace Corsinvest.VisualStudio.Agents.Menu;
